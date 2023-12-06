7c18a4262fe02a940ca8e4edd2b57c5ae966b90e
TrikKH <ejames711@gmail.com>
2023-12-06 07:05:32 -0600

    setup init Pocketbase connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import '@picocss/pico'
 import './App.css'
+import pb from './lib/pocketbase';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  async function getRecords(){
+    pb.autoCancellation(false);
+    const record = await pb.collection('test').getOne('RECORD_ID', {
+      expand: 'relField1,relField2.subRelField',
+  });
+  console.log(record)
+  }
+
+  useEffect(() => {
+    getRecords()
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div></div>
     </>
   )
 }
