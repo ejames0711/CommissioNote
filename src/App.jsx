675199b54426866bc2ba3a84d70b3d759816dbf9
TrikKH <ejames711@gmail.com>
2023-12-13 20:18:03 -0600

    create initial layout for device cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import { useEffect, useState } from 'react'
 import '@picocss/pico'
 import './App.css'
-
-
-import PocketBase from 'pocketbase'
+import logo from "./assets/crestron.png"
+import pb from './lib/pocketbase.js'
 
 
 function App() {
 
-  const pb = new PocketBase('POCKETBASE_CLIENT')
   const [records,setRecords] = useState([])
 
   async function getData(){
-    await pb.collection('devices').getFullList().then((res) => setRecords(res));
-    console.log(records)
+    await pb.collection('test').getFullList().then((res) => setRecords(res))
   }
 
   useEffect(() => {
     getData()
   },[])
 
-  
-
   return (
     <>
-      <div>
-        {records.map((device) => (
-          <div key={device.id}>{device.id}</div>
-        ))}
-      </div>
+      <h1>CommissioNote</h1>
+      {records.map((device) => (
+        <article className='container card' key={device.id}>
+         <h3 >{device.field.Model}</h3>
+         <img src={logo} alt="" />
+          <p><strong>Brand:</strong> {device.field.Brand}</p>
+          <p><strong>Default Address:</strong> {device.field["Default IP/Subnet"]}</p>
+          <p><strong>Default Login:</strong> {device.field["Default Credentials"]}</p>
+          <p><strong>Config Software:</strong> {device.field["Config Software"]}</p>
+          <p><strong>Support:</strong> {device.field["Support Number"]} | {device.field["Support Email"]}</p>
+          <button className='outline'>More Info</button>
+        </article>
+      ))}
     </>
   )
 }
